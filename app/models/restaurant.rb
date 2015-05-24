638985e413d21eb7b4e8cafac662fb2e194c7a9d
ZacharyWeiner <zach@cloud.com>
2015-05-24 02:10:54 -0400

    fixed errors around not having a user or logged in and reset database

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,7 @@ class Restaurant < ActiveRecord::Base
 	validates :name, :address, :phone, :website, :image, presence: true
 	validates :phone, format: {with: /\A\(\d{3}\) \d{3}-\d{4}\z/, message: "must be in the format (123) 456-7890" }
 	validates :website, format: {with: /\Ahttps?:\/\/.*\z/, message: " must start with http:// or https://" }
-	validates :website, format: {with: /\A\d+[^,]+,[^,]+, [A-z]{2}, \d{5}\z/, message: " must be '111 some road, Some City, NY, 10001' " }
+	validates :address, format: {with: /\A\d+[^,]+,[^,]+, [A-z]{2}, \d{5}\z/, message: " must be '111 some road, Some City, NY, 10001' " }
 
 	
 end
